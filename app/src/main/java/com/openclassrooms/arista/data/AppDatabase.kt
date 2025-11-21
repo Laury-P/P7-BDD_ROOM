608938670d0a5414486516ec4cf570d7c51f3224
Laury PRIN <prinlauryy@gmail.com>
2025-11-21 10:49:27 +0100

    Création d'un dataset pour le sommeil et l'utilisateur

--- app/src/main/java/com/openclassrooms/arista/data/AppDatabase.kt
+++ app/src/main/java/com/openclassrooms/arista/data/AppDatabase.kt
@@ -4,12 +4,17 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.openclassrooms.arista.data.dao.ExerciseDtoDao
 import com.openclassrooms.arista.data.dao.SleepDtoDao
 import com.openclassrooms.arista.data.dao.UserDtoDao
 import com.openclassrooms.arista.data.entity.ExerciseDto
 import com.openclassrooms.arista.data.entity.SleepDto
 import com.openclassrooms.arista.data.entity.UserDto
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 @Database(
     entities = [UserDto::class, SleepDto::class, ExerciseDto::class],
@@ -20,6 +25,16 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun sleepDao(): SleepDtoDao
     abstract fun exerciseDao(): ExerciseDtoDao
 
+    private class AppDatabaseCallback( private val scope: CoroutineScope) : Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.userDao(), database.sleepDao())
+                }
+            }
+        }
+    }
 
 
     companion object {
@@ -38,6 +53,32 @@ abstract class AppDatabase : RoomDatabase() {
                 instance
             }
         }
+
+        suspend fun populateDatabase(userDao: UserDtoDao, sleepDao: SleepDtoDao) {
+            sleepDao.insertSleep(
+                SleepDto(
+                    startTime = LocalDateTime.now().minusDays(1).atZone(ZoneOffset.UTC).toInstant().toEpochMilli(),
+                    duration = 480,
+                    quality = 4
+                )
+            )
+            sleepDao.insertSleep(
+                SleepDto(
+                    startTime = LocalDateTime.now().minusDays(2).atZone(ZoneOffset.UTC).toInstant().toEpochMilli(),
+                    duration = 450,
+                    quality = 3
+                )
+
+            )
+            userDao.insertUser(
+                UserDto(
+                    name = "John Doe",
+                    email ="john.doe@exemple.com",
+                    password = "1234"
+                )
+            )
+        }
+
 
     }
 
