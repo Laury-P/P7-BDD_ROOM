077c0a5f925f04a8c11735b9496e2fbe76bfa452
Laury PRIN <prinlauryy@gmail.com>
2025-11-21 10:36:47 +0100

    Ajout des fonctions insert dans userdao ou sleepdao

--- app/src/main/java/com/openclassrooms/arista/data/dao/SleepDtoDao.kt
+++ app/src/main/java/com/openclassrooms/arista/data/dao/SleepDtoDao.kt
@@ -1,14 +1,16 @@
 package com.openclassrooms.arista.data.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import com.openclassrooms.arista.data.entity.SleepDto
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SleepDtoDao {
+    @Insert
+    suspend fun insertSleep(sleep: SleepDto): Long
 
     @Query("SELECT * FROM sleep")
     fun getAllSleep(): Flow<List<SleepDto>>
-
 }
--- app/src/main/java/com/openclassrooms/arista/data/dao/UserDtoDao.kt
+++ app/src/main/java/com/openclassrooms/arista/data/dao/UserDtoDao.kt
@@ -1,12 +1,15 @@
 package com.openclassrooms.arista.data.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import com.openclassrooms.arista.data.entity.UserDto
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UserDtoDao {
+    @Insert
+    suspend fun insertUser(user: UserDto): Long
 
     @Query("SELECT * FROM user")
     fun getUser(): Flow<List<UserDto>>
