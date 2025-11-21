0c4035f4fe0e10c27dd66017ff5ab4ea592cd436
Laury PRIN <prinlauryy@gmail.com>
2025-11-21 10:16:19 +0100

    Ajout des Dto

---
+++ app/src/main/java/com/openclassrooms/arista/data/entity/ExerciceDto.kt
@@ -0,0 +1,40 @@
+package com.openclassrooms.arista.data.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import com.openclassrooms.arista.domain.model.ExerciseCategory
+import java.time.LocalDateTime
+
+@Entity(
+    tableName = "exercise",
+    foreignKeys = [
+        ForeignKey(
+            entity = UserDto::class,
+            parentColumns = ["id"],
+            childColumns = ["user_id"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+data class ExerciceDto(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Long = 0,
+
+    @ColumnInfo(name = "start_time")
+    var startTime: LocalDateTime,
+
+    @ColumnInfo(name = "duration")
+    var duration: Int,
+
+    @ColumnInfo(name = "category")
+    var category: ExerciseCategory,
+
+    @ColumnInfo(name = "intensity")
+    var intensity: Int,
+
+    @ColumnInfo(name = "user_id")
+    val userId: Long,
+)
---
+++ app/src/main/java/com/openclassrooms/arista/data/entity/SleepDto.kt
@@ -0,0 +1,36 @@
+package com.openclassrooms.arista.data.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import java.time.LocalDateTime
+
+@Entity(
+    tableName = "sleep",
+    foreignKeys = [
+        ForeignKey(
+            entity = UserDto::class,
+            parentColumns = ["id"],
+            childColumns = ["user_id"],
+            onDelete = ForeignKey.CASCADE,
+        )
+    ]
+)
+data class SleepDto(
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    val id: Long = 0,
+
+    @ColumnInfo(name = "start_time")
+    var startTime: LocalDateTime,
+
+    @ColumnInfo(name = "duration")
+    var duration: Int,
+
+    @ColumnInfo(name = "quality")
+    var quality: Int,
+
+    @ColumnInfo(name = "user_id")
+    val userId: Long,
+)
