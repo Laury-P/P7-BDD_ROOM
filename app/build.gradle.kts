17932b410edce4023cca64e6edc075dbcea62d65
Laury PRIN <prinlauryy@gmail.com>
2025-11-25 15:09:25 +0100

    Modification du gradle au profit de mockk plutot que mockito

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation("io.mockk:mockk:1.13.9")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0-RC2")
+    testImplementation("app.cash.turbine:turbine:1.0.0")
+
 }
 
 
